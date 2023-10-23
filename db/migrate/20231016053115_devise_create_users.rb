635105107a6cd081a2ba0d12c71a4ae8089cba52
t-kashu <146803701+t-kashu@users.noreply.github.com>
2023-10-23 11:44:40 +0900

    Update 20231016053115_devise_create_users.rb

--- db/migrate/20231016053115_devise_create_users.rb
+++ db/migrate/20231016053115_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.string :encrypted_password, null: false, default: ""
       t.string :name,               null: false
       t.text :profile,              null: false
-      t.text :occupatin,            null: false
+      t.text :occupation,            null: false  # 修正: occupatin から occupation に変更
       t.text :position,             null: false
 
       ## Recoverable
